6f2f78470b2dd4850c2999ae391e3a21df88a157
Luis Gaytan <agaytan@techiedodo.com>
2018-07-10 15:36:49 -0700

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -11,17 +11,6 @@ import './styles/styles.sass';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: "Water Bill", amount: 15000, createdAt: 1525201200000}));
-store.dispatch(addExpense({description: "Gas Bill", amount: 25000, createdAt: 1522782000000}));
-// store.dispatch(filterExpense('Gas'));
-
-store.dispatch(sortByAmount());
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
